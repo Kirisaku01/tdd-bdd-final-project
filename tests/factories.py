11ff1962f6daf5323690b8d03704a6e9e53a52df
Kirisaku01 <153277051+Kirisaku01@users.noreply.github.com>
2024-01-16 13:37:20 +0700

    Update factories.py

--- tests/factories.py
+++ tests/factories.py
@@ -32,3 +32,15 @@ class ProductFactory(factory.Factory):
 
     id = factory.Sequence(lambda n: n)
    ## Add code to create Fake Products 
+    def test_read_a_product(self):
+        """It should Read a Product"""
+        product = ProductFactory()
+        product.id = None
+        product.create()
+        self.assertIsNotNone(product.id)
+        # Fetch it back
+        found_product = Product.find(product.id)
+        self.assertEqual(found_product.id, product.id)
+        self.assertEqual(found_product.name, product.name)
+        self.assertEqual(found_product.description, product.description)
+        self.assertEqual(found_product.price, product.price)
